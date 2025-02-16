39a797c9e764a920a6dd886f4f711ebe01feaf2e
julioCK <bocumaa@gmail.com>
2025-02-16 09:25:13 -0300

    project created

---
+++ src/main/java/udemy/javaspring/udemyJavaNA22/UdemyJavaNa22Application.java
@@ -0,0 +1,13 @@
+package udemy.javaspring.udemyJavaNA22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyJavaNa22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyJavaNa22Application.class, args);
+	}
+
+}
---
+++ src/test/java/udemy/javaspring/udemyJavaNA22/UdemyJavaNa22ApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.javaspring.udemyJavaNA22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyJavaNa22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
